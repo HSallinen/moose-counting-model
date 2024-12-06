51e48e04c837c4e8e2577b4cd3c5fbec2c9bbc52
Hermanni Sallinen <sallinenhermanni@gmail.com>
2024-12-06 11:41:44 +0200

    Improve moose struct and add moose child struct

--- src/moose.rs
+++ src/moose.rs
@@ -1,5 +1,12 @@
 struct Moose {
+    id: i32,
     cordinates: (i32, i32),
-    restcenter: (i32, i32),
-    
+    rest_center: (i32, i32, i32),
+    feeding_areas: Vec<(i32, i32, i32)>,
+}
+
+struct MooseChild {
+    id: i32,
+    cordinates: (i32, i32),
+    parent_id: i32,
 }
