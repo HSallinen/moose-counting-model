3e7b731cda284f662c711770a40d4aa8cec370ac
Hermanni Sallinen <sallinenhermanni@gmail.com>
2024-12-06 11:42:43 +0200

    Make moose structures public

--- src/moose.rs
+++ src/moose.rs
@@ -1,11 +1,11 @@
-struct Moose {
+pub struct Moose {
     id: i32,
     cordinates: (i32, i32),
     rest_center: (i32, i32, i32),
     feeding_areas: Vec<(i32, i32, i32)>,
 }
 
-struct MooseChild {
+pub struct MooseChild {
     id: i32,
     cordinates: (i32, i32),
     parent_id: i32,
